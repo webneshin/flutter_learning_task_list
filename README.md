2601e5d5079b93da1e4cdd1b43565f7ee3ea1d81
webneshin <sajjad.eb@gmail.com>
2023-08-01 18:24:36 +0330

    Init project

---
+++ README.md
@@ -0,0 +1,3 @@
+# task_list
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/webneshin/task_list/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.webneshin.task_list
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
